f97ad77834dc92b0380961a9ba251c9cd41f1f77
addster1018 <addster1018@gmail.com>
2019-03-08 02:56:28 -0600

    Added GUI

--- WeatherApp.py
+++ WeatherApp.py
@@ -1,6 +1,7 @@
 import urllib
 from urllib.request import urlopen
 import ast
+import tkinter as tk
 
 url = "http://api.openweathermap.org/data/2.5/weather?zip=61801,us&APPID=d556e90c6c6276a06c70a7a304572772"
 
@@ -14,4 +15,14 @@ page_dict = ast.literal_eval(the_page.decode('UTF-8'))
 
 temp_F = (float(page_dict['main']['temp']) - 273.15) * 9/5 + 32
 
-print("Temp: " + str(temp_F) + " degrees Fahrenheit")
+out_str = "Temp: " + str(temp_F) + " degrees Fahrenheit"
+
+m=tk.Tk(className = "WEATHER")
+
+# widgets
+text = tk.Message(m, text = out_str)
+text.pack()
+button = tk.Button(m, text='Stop', width=100, command=m.destroy)
+button.pack()
+
+m.mainloop()
